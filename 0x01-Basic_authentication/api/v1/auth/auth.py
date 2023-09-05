1a47a915aa682fa94c504bffccbe1ccbd85354ef
Boomni Jonathan <rejoiceoye1@gmail.com>
2023-09-05 13:05:54 +0100

    Implemented Auth class

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""API Authentication"""
+from flask import request
+from typing import TypeVar, List
+
+
+class Auth():
+    """Manages the API authentication"""
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """Returns a boolean"""
+        if path and excluded_paths:
+            return False
+    
+    def authorization_header(self, request=None) -> str:
+        """Returns None - request"""
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """Returns None - request"""
+        return None
