974f5082cbb954ccc60753b53bddb641734b7aa6
Boomni Jonathan <rejoiceoye1@gmail.com>
2023-09-07 21:21:36 +0000

    Implemented Basic - User object

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,8 @@
 from api.v1.auth.auth import Auth
 import base64
 import binascii
+from models.user import User
+from typing import TypeVar
 
 
 class BasicAuth(Auth):
@@ -55,7 +57,7 @@ class BasicAuth(Auth):
             return None, None
         username, password = decoded_base64_authorization_header.split(":")
         return username, password
-    
+
     def user_object_from_credentials(
             self,
             user_email: str,
@@ -65,6 +67,15 @@ class BasicAuth(Auth):
             return None
         if user_pwd is None or not isinstance(user_pwd, str):
             return None
+
+        users = User.search({'email': user_email})
+        if not users:
+            return None
+
+        user = users[0]
+        if not user.is_valid_password(user_pwd):
+            return None
+        return user
 
     def current_user(self, request=None) -> TypeVar('User'):
         """Basic - Overload current_user"""
