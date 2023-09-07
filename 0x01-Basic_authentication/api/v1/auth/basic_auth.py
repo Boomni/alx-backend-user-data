5b3d35ea6f44b906e1b686e286dd5549fd8d8624
Boomni Jonathan <rejoiceoye1@gmail.com>
2023-09-07 04:02:39 +0000

    Implemented Basic - User credentials

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """API Authentication"""
 from api.v1.auth.auth import Auth
+from models.user import User
 
 
 class BasicAuth(Auth):
@@ -23,3 +24,17 @@ class BasicAuth(Auth):
             return authorization_header[6:]
         else:
             return None
+
+    def extract_user_credentials(
+            self,
+            decoded_base64_authorization_header: str
+            ) -> str:
+        """Basic - Base64 decode"""
+        if decoded_base64_authorization_header is None:
+            return None, None
+        if not isinstance(decoded_base64_authorization_header, str):
+            return None, None
+        if ":" not in decoded_base64_authorization_header:
+            return None, None
+        username, password = decoded_base64_authorization_header.split(":")
+        return username, password
