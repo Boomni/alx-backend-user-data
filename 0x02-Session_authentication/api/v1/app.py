fa1d7faa48626a0cf57576868dc09589414edf5e
Boomni Jonathan <rejoiceoye1@gmail.com>
2023-09-13 02:21:28 +0000

    Implemented def session_cookie(self, request=None): that returns a cookie value from a request

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -57,19 +57,17 @@ def check_auth() -> str:
                 '/api/v1/status/',
                 '/api/v1/unauthorized/',
                 '/api/v1/forbidden/',
-                'api/v1/auth_session/login'
+                '/api/v1/auth_session/login'
                 ]
         if auth.require_auth(request.path, excluded_paths):
             auth_header = auth.authorization_header(request)
-            if auth_header is None:
-                abort(401)
-            user = auth.current_user(request)
-            request.current_user = user
-            if user is None:
-                abort(403)
             auth_cookie = auth.session_cookie(request)
+            auth_user = auth.current_user(request)
+            request.current_user = user
             if auth_header is None and auth_cookie is None:
                 abort(401)
+            if auth_user is None:
+                abort(403)
 
 
 if __name__ == "__main__":
