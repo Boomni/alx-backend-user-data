8d11c84b570f52dc3ae6545f73a50e08e325eab2
Boomni Jonathan <rejoiceoye1@gmail.com>
2023-09-13 01:36:55 +0000

    completed the implementation of my user_id_for_session_id method in SessionAuth

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -7,7 +7,9 @@ import uuid
 class SessionAuth(Auth):
     """ Session authentication class
     """
-    user_id_by_session_id = {}
+    def __init__(self):
+        """class instance attribute"""
+        self.user_id_by_session_id = {}
 
     def create_session(self, user_id: str = None) -> str:
         """creates a Session ID for a user_id"""
@@ -19,9 +21,6 @@ class SessionAuth(Auth):
 
     def user_id_for_session_id(self, session_id: str = None) -> str:
         """returns a User ID based on a Session ID"""
-        if session_id is None or not isinstance(session_id, str ):
+        if session_id is None or not isinstance(session_id, str):
             return None
-        """
-        Return the value (the User ID) for the key session_id in the dictionary user_id_by_session_id.
-        You must use .get() built-in for accessing in a dictionary a value based on key
-        """
+        return self.user_id_by_session_id.get(session_id)
